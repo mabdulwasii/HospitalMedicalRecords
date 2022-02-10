b3a8e25a80deec17d9d2f33ef832f57bbd8b0a8d
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-02-10 08:28:36 +0100

    initial commit

---
+++ src/main/java/org/princely/hospitalmedicalrecords/HospitalMedicalRecordsApplication.java
@@ -0,0 +1,13 @@
+package org.princely.hospitalmedicalrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMedicalRecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalMedicalRecordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/princely/hospitalmedicalrecords/HospitalMedicalRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.princely.hospitalmedicalrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMedicalRecordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
