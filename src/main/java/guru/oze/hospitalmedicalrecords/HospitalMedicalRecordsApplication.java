4fb28bb824e7ae9cb14b7a23ff580549678bb6ed
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-02-13 15:09:03 +0100

    package refactoring

--- src/main/java/guru/oze/hospitalmedicalrecords/HospitalMedicalRecordsApplication.java
+++ src/main/java/guru/oze/hospitalmedicalrecords/HospitalMedicalRecordsApplication.java
@@ -1,9 +1,12 @@
-package org.princely.hospitalmedicalrecords;
+package guru.oze.hospitalmedicalrecords;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
 public class HospitalMedicalRecordsApplication {
 
     public static void main(String[] args) {
--- src/test/java/guru/oze/hospitalmedicalrecords/HospitalMedicalRecordsApplicationTests.java
+++ src/test/java/guru/oze/hospitalmedicalrecords/HospitalMedicalRecordsApplicationTests.java
@@ -1,4 +1,4 @@
-package org.princely.hospitalmedicalrecords;
+package guru.oze.hospitalmedicalrecords;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
